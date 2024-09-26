12b18b2cf5ec807a5a1e513a3e8ae8a522d152e6
sanam453 <sanam.hamza9@gmail.com>
2024-09-26 08:44:46 +0430

    Added Navbar Section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
 import type { Config } from "tailwindcss";
+import { mtConfig } from "@material-tailwind/react";
+
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#0BB96D",
+        secondary: "#343434"
       },
     },
   },
-  plugins: [],
+  plugins: [mtConfig],
 };
 export default config;
